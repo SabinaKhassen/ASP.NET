2e43de8b60c9b313eb9cc0c517c9aaaacb75e14c
ХасенС <@DESKTOP-3LILO2K>
2019-08-21 19:01:08 +0600

    Добавьте файлы проекта.

---
+++ ASP.NET/Controllers/AuthorController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET: Author
+        public ActionResult Index()
+        {
+            List<Authors> authors;
+            using (Model1 db = new NET.Model1())
+            {
+                authors = db.Authors.ToList();
+            }
+            return View(authors);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Authors author)
+        {
+            using (Model1 db = new Model1())
+            {
+                db.Authors.Add(author);
+                db.SaveChanges();
+            }
+            return Redirect("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Authors author;
+            using(Model1 db = new Model1())
+            {
+                if (id != 0)
+                {
+                    ViewBag.Message = "Edit";
+                    author = db.Authors.Where(a => a.Id == id).FirstOrDefault();
+                }
+                else
+                {
+                    ViewBag.Message = "Create";
+                    author = null;
+                }
+            }
+            return View(author);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Authors author)
+        {
+            using (Model1 db = new Model1())
+            {
+                Authors auth = db.Authors.Where(a => a.Id == author.Id).FirstOrDefault();
+                if (auth != null)
+                {
+                    auth.FirstName = author.FirstName;
+                    auth.LastName = author.LastName;
+                }
+                else
+                    db.Authors.Add(author);
+                db.SaveChanges();
+            }
+            return RedirectToActionPermanent("Index", "Author");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Authors author;
+            using (Model1 db = new Model1())
+            {
+                author = db.Authors.Where(a => a.Id == id).FirstOrDefault();
+                db.Authors.Remove(author);
+                db.SaveChanges();
+            }
+            return RedirectToActionPermanent("Index", "Author");
+        }
+    }
+}
---
+++ ASP.NET/Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            List<Books> books;
+            using (Model1 db = new Model1())
+            {
+                books = db.Books.ToList();
+            }
+            return View(books);
+        }
+    }
+}
